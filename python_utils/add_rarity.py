27700374739c44f2515a0fdf5a6ac93974030254
mohammed.jalil@gmail.com <mo@Mohammeds-MacBook-Pro.local>
2022-04-12 02:20:33 +0800

    added file rename

--- python_utils/add_rarity.py
+++ python_utils/add_rarity.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 import os, sys, getopt
-path = ""
+def rename_file_folder(input, output, input_path=os.getcwd() + '/layers', rename=False):
+   if rename is True:
+      print("Rename flag is set")
+      input_path = os.path.join(input_path, input)
+      output_path = os.path.join(input_path, output)
+      processed = f"Renamed {input_path} --> {output_path}"
+      
+   if rename is not True:
+      print("Rename flag is set to False, not renaming")
 
-def list_directory(input_path):
+def list_directory(input_path=''):
    
    print("Running list Directory")
    if input_path == '' :
-      input_path = os.getcwd();
+      working_path = os.getcwd() + '/layers'
+   elif input_path != '':
+      working_path = os.getcwd() + '/layers/' + input_path
    
-   print("input path is ", input_path)
-   list_directory = os.listdir(input_path)
+   print("Working path is ", working_path)
+   list_directory = os.listdir(working_path)
    for file_name in list_directory:
       print("Processing: ", file_name)
-      new_name = add_rarity(file_name)
-      processed = f"Converted {file_name} --> {new_name}"
-      print(processed)
+      rarity_name = add_rarity(file_name)
+      # processed = f"Converted {file_name} --> {rarity_name}"
+      # print(processed)
 
-def add_rarity(name, rarity=0):
+def add_rarity(name, rarity=0, type='file'):
    if rarity == 0:
       rarity = 10
-   rarity_name = name + '#' + str(rarity)
+   
+   if type == 'folder':
+      rarity_name = name + '#' + str(rarity)
+   if type == 'file':
+      rarity_name = str(name[:name.find('.')]) + '#' + str(rarity) + str(name[name.find('.'):])
+    
+
    print("New name is ", rarity_name)
+   
    return rarity_name
 
 
