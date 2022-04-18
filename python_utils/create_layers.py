0a7cc5e864d5b568af0869ce16359432069857a2
mohammed.jalil@gmail.com <mo@Mohammeds-MacBook-Pro.local>
2022-04-18 19:00:46 +0800

    added function to list files, directories and traverse source directory

--- python_utils/create_layers.py
+++ python_utils/create_layers.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-import os, sys, getopt , shutil
+import os, sys, getopt , shutil, fnmatch
 
 # cp /Volumes/GoogleDrive/Shared\ drives/DESIGN/OpenFren/Character\ Art/Layering/Canine/Latest\ Final/Cloth/Cloth_05/Cloth_05_Third_N.png ./layers/Cloth/Cloth_05\#15/3_Tertiary
 
 # input_path = '/Volumes/GoogleDrive/Shared\ drives/DESIGN/OpenFren/Character\ Art/Layering/Canine/Latest\ Final/Cloth/Cloth_05/Cloth_05_Third_N.png'
-input_path = '/Volumes/GoogleDrive/My Drive/brand_partnerships.csv'
+input_path = '/Volumes/GoogleDrive/My Drive/'
 output_path = os.getcwd() + '/.test'
 
 # There are a number of ways to copy files in python, see: https://stackoverflow.com/questions/123198/how-to-copy-files
@@ -30,9 +30,24 @@ def copy_files(source, destination):
 # 2. Iterate over files in Directory
 # 3. Move files in directory
 
+# Getting all files and subdirectories in a directory: https://stackoverflow.com/questions/2909975/python-list-directory-subdirectory-and-files
+# os.listdir only give files and folders on the current directory
+def list_directory(input_path):
+    # list_directory = os.listdir(input_path)
+    # for file_name in list_directory:
+    #     print(f'{file_name} in {input_path}')
+    file_pattern = "*.png"
+
+    for path, subdirs, files in os.walk(input_path):
+        for name in files:
+            if fnmatch.fnmatch(name, file_pattern):
+                print(f'Name is {name}')
+                print(f'Path is {path}')
+                print(os.path.join(path, name))
 
 def main(argv):
-    copy_files(input_path, output_path)
+    list_directory(input_path)
+    # copy_files(input_path, output_path)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
