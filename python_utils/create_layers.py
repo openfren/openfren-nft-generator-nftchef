0e02090d84d897e2bde85bd79ba149af724fc82c
mohammed.jalil@gmail.com <mo@Mohammeds-MacBook-Pro.local>
2022-04-19 12:18:11 +0800

    updated with auto download and naming for the card

--- python_utils/create_layers.py
+++ python_utils/create_layers.py
@@ -9,20 +9,21 @@ canine_path = '/Volumes/GoogleDrive/Shared drives/DESIGN/OpenFren/Character Art/
 card_path = '/Volumes/GoogleDrive/Shared drives/DESIGN/OpenFren/Character Art/Layering/Card/'
 input_path = '/Volumes/GoogleDrive/Shared drives/DESIGN/OpenFren/Character Art/Layering/'
 output_path = os.getcwd() + '/.utils_output/'
+csv_card_path = './downloaded_files/Card_Map.csv'
 
 # There are a number of ways to copy files in python, see: https://stackoverflow.com/questions/123198/how-to-copy-files
 # Resource 2, https://www.geeksforgeeks.org/python-shutil-copy-method/
-def copy_files(source, destination):
+def copy_files(source, destination, destination_file_name):
     print(f'Copying from {source} to {destination}')
 
     if not os.path.exists(destination):
-        os.mkdir(destination)
+        os.makedirs(destination)
     
     # This needs linux back slahes / escape characters to work 
     # dest = os.system(f'cp {source} {destination}')
 
     # The below alternative is better as it is portable and a native library in python. Have to remove backslahes
-    dest = shutil.copy(source, destination, follow_symlinks=True)
+    dest = shutil.copy(source, destination + destination_file_name, follow_symlinks=True)
 
     print(f'Files copied to {dest}')
 
@@ -42,6 +43,21 @@ def list_directory(input_path):
                 # print(f'Name is {name}')
                 # print(f'Path is {path}')
                 print(os.path.join(path, name))
+
+def read_csv_input_files(input_path):
+    f = open(input_path, 'r')
+    reader = csv.reader(f)
+    header = []
+    header = next(reader)
+    for row in reader:
+        source_file = row[0] + '/' + row[1]
+        destination_folder = output_path + 'layers/' + row[7]
+        destination_file = row[8]
+        print('Copying ', row[1] , 'to' , destination_file)
+        copy_files(source_file, destination_folder,destination_file)
+
+def read_spreadsheet(input_path):
+    return 
 
 def create_csv_input_files(input_path, type=""):
     file_pattern = "*.png"
@@ -69,7 +85,7 @@ def create_csv_input_files(input_path, type=""):
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"hl:c:")
+        opts, args = getopt.getopt(argv,"htl:c:")
     except getopt.GetoptError:
         print('There was an error')
         print('create_layers.py -l card --> creates csv file with list of files for card')
@@ -90,10 +106,17 @@ def main(argv):
                 create_csv_input_files(goblin_path, 'goblin')
         if opt in ['-c']:
             print('Creating layers for ', arg)
-
-    # create_csv_input_files(input_path)
-    # list_directory(input_path)
-    # copy_files(input_path, output_path)
+            read_csv_input_files(csv_card_path)
+        if opt in ['-t']:
+            print('Testing')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
+# Testing
+# s = '/Volumes/GoogleDrive/Shared drives/DESIGN/OpenFren/Character Art/Layering/Card/Back Design/Goblin_B.png'
+# d = '/Users/mo/code/openfren/openfren-nft-generator-nftchef/.utils_output/layers/Card_Back/1_Text_Black/'
+# f = 'Black Goblin Text#10.png'
+# copy_files(s,d,f)
+
+# read_csv_input_files(csv_card_path)
