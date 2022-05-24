1c7489870fb60f50185119087c5864ec68c82fce
mohammed.jalil@gmail.com <mo@Mohammeds-MacBook-Pro.local>
2022-05-24 22:26:44 +0800

    created script to remove traits

---
+++ python_utils/remove_traits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import subprocess
+# subprocess.run(["ls", "-l"])
+
+traits = ["LineArt", "1_Primary", "4_LineArt", "1_Layer", "2_Layer", "Multiply"]
+
+for trait in traits:
+    print(f'Removing {trait} from json')
+    subprocess.run(["node", "/Users/mo/code/openfren/openfren-nft-generator-nftchef/utils/removeTrait.js", trait])
+    print(f'Removing {trait} from Solana json')
+    subprocess.run(["node", "/Users/mo/code/openfren/openfren-nft-generator-nftchef/utils/removeTraitSolana.js", trait])
